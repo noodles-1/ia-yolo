3dee2e972f203b8a04284b77a701a6ff0396610b
noodles-1 <adrianeroa081202@gmail.com>
2024-05-10 07:49:45 +0800

    detection now working

---
+++ scripts/voc_annotation.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import argparse
+import xml.etree.ElementTree as ET
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from os import listdir
+
+def convert_voc_annotation(data_path, anno_path, use_difficult_bbox=False):
+
+    classes = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+               'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
+               'motorbike', 'person', 'pottedplant', 'sheep', 'sofa',
+               'train', 'tvmonitor']
+    # classes = ['person', 'car', 'bus', 'bicycle',  'motorbike']
+    img_inds_file = os.path.join(data_path, 'JPEGImages')
+    image_inds = []
+    for file_name in listdir(img_inds_file):
+        image_inds.append(file_name[:len(file_name)-4])
+
+    with open(anno_path, 'w') as f:
+        for image_ind in image_inds:
+            image_path = os.path.join(data_path, 'JPEGImages', image_ind + '.jpg')
+            annotation = image_path
+            label_path = os.path.join(data_path, 'Annotations', image_ind + '.xml')
+            root = ET.parse(label_path).getroot()
+            objects = root.findall('object')
+            for obj in objects:
+                difficult = obj.find('difficult').text.strip()
+                if (not use_difficult_bbox) and(int(difficult) == 1):
+                    continue
+                bbox = obj.find('bndbox')
+                if obj.find('name').text.lower().strip() in classes:
+                    class_ind = classes.index(obj.find('name').text.lower().strip())
+                    xmin = bbox.find('xmin').text.strip()
+                    xmax = bbox.find('xmax').text.strip()
+                    ymin = bbox.find('ymin').text.strip()
+                    ymax = bbox.find('ymax').text.strip()
+                    annotation += ' ' + ','.join([xmin, ymin, xmax, ymax, str(class_ind)])
+
+            print(annotation)
+            f.write(annotation + "\n")
+    return len(image_inds)
+
+if __name__ == '__main__':
+    # for foggy conditions
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", default="../datasets")
+    parser.add_argument("--train_annotation", default="data/dataset_fog/voc_norm_train.txt")
+    parser.add_argument("--test_annotation",  default="data/dataset_fog/voc_norm_test.txt")
+    parser.add_argument("--val_annotation",  default="data/dataset_fog/voc_norm_val.txt")
+
+    flags = parser.parse_args()
+
+    num1 = convert_voc_annotation(os.path.join(flags.data_path, 'voctrain'), flags.train_annotation, False)
+    num2 = convert_voc_annotation(os.path.join(flags.data_path, 'vocval'), flags.val_annotation, False)
+    num3 = convert_voc_annotation(os.path.join(flags.data_path, 'voctest'), flags.test_annotation, False)
+    print('=> The number of image for train is: %d\tThe number of image for val is:%d\tThe number of image for test is:%d' %(num1, num2, num3))
+
+
+
